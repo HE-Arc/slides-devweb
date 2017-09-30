3355e86c99d39982d0eef871e76ff1be0807fe3b
Yoan Blanc <yoan@dosimple.ch>
2017-09-30 17:22:35 -0400

    Auto deploy to github pages.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -xe
+
+make build/index.html
+
+(
+    cd build
+    rm -f *.tmp *.md
+    git init
+    git config user.name "TravisCI"
+    git config user.email "travis@he-arc.test"
+    git add .
+    git commit -m "Deployed to github pages"
+    git push -f -q "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" master:gh-pages
+)
