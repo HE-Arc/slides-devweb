0c8f6c613a4450b13b716c404a5b85e46f0db769
Yoan Blanc <yoan@dosimple.ch>
2017-10-24 05:02:27 -0700

    sed des familles.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ make build/index.html
 (
     cd build
     rm -f *.tmp *.md
+    find . -iname "*.html" -exec sed -i "s/http%3A/https%3A/g" {} \;
     git init
     git config user.name "TravisCI"
     git config user.email "travis@he-arc.test"
