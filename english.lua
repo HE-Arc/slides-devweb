e06e43d205f996b67e5ec6d5e1007c3991ac6281
Yoan Blanc <yoan@dosimple.ch>
2017-11-18 11:05:17 +0100

    Lua filter that do not alter the metadatas.
    - the fonts were mixed...
    
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- english.lua
+++ english.lua
@@ -1,28 +1,38 @@
--- new elements
-
 local lang = 'english'
 
 local before = pandoc.RawInline('tex', '\\begin{otherlanguage}{' .. lang .. '}')
 local after = pandoc.RawInline('tex', '\\end{otherlanguage}')
 
-return {
-  {
-    Meta = function(meta)
-      meta['babel-otherlangs'] = lang
-      meta['polyglossia-otherlangs'] = {name = lang}
-      return meta
-    end,
+local meta = {}
 
-    Code = function(code)
+return {
+  -- remove the metadata
+  {
+    Meta = function (el)
+      meta = el
+      return {}
+    end
+  },
+  -- modify any code element
+  {
+    Code = function (code)
       return {before, code, after}
     end,
 
-    CodeBlock = function(code_block)
+    CodeBlock = function (code_block)
       return {pandoc.Para({before}), code_block, pandoc.Para({after})}
     end,
 
-    RawBlock = function(raw_block)
+    RawBlock = function (raw_block)
       return {pandoc.Para({before}), raw_block, pandoc.Para({after})}
     end,
+  },
+  -- restore the metadata
+  {
+    Meta = function(_)
+      meta['babel-otherlangs'] = lang
+      meta['polyglossia-otherlangs'] = {name = lang}
+      return meta
+    end
   }
 }
